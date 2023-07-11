42dcf1f5e2ee22542641fd9826f9c0a097384b57
Byron Mandela <mandelabyron@gmail.com>
2023-07-11 22:00:49 +0300

    first commit

---
+++ bodyshop/bodyshop.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from bodyshop.spiders.crawler import CrawlerSpider
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+def main():
+    settiings=get_project_settings()
+    process = CrawlerProcess(settings=settiings)
+    
+    process.crawl(CrawlerSpider)
+    process.start()
+    
+if __name__ == '__main__':
+    main()
